2fa60cadcd23f67f925904d4b99ea2212f9cebed
lab_manjaro <labpc2019@gmail.com>
2021-05-11 10:40:11 +0900

    add output conv

--- model/Ghost_Mix.py
+++ model/Ghost_Mix.py
@@ -10,15 +10,18 @@ from torchsummary import summary
 class GhostMix(torch.nn.ModuleList):
 
     def __init__(self, input_ch: int, output_ch: int, *args,
-                 feature_num: int=64, layer_num: int=9, **kwargs):
+                 feature_num: int=64, block_num: int=9, **kwargs):
         super(GhostMix, self).__init__()
         activations = {'relu': ReLU, 'leaky': Leaky, 'swish': Swish, 'mish': Mish}
         activation = kwargs.get('activation', 'relu').lower()
         mode = kwargs.get('mode', 'normal')
-        self.start_conv = torch.nn.Conv2d(input_ch, output_ch, 3, 1, 1)
+        ratio = kwargs.get('ratio', 2)
+        self.start_conv = torch.nn.Conv2d(input_ch, feature_num, 3, 1, 1)
         self.start_activation = activations[activation]()
-        self.ghost_layers = torch.nn.ModuleList([Ghost_layer(output_ch, output_ch, mode=mode) for _ in range(layer_num)])
-        self.activations = torch.nn.ModuleList([activations[activation]() for _ in range(layer_num)])
+        self.ghost_layers = torch.nn.ModuleList([Ghost_layer(feature_num, feature_num, 
+                                                             mode=mode, ratio=ratio) for _ in range(block_num)])
+        self.activations = torch.nn.ModuleList([activations[activation]() for _ in range(block_num)])
+        self.output_conv = torch.nn.Conv2d(feature_num, output_ch, 3, 1, 1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
@@ -27,6 +30,7 @@ class GhostMix(torch.nn.ModuleList):
         for i, (layer, activation) in enumerate(zip(self.ghost_layers, self.activations)):
             x = activation(layer(x))
             x = x + x_in
+        x = self.output_conv(x)
         return x
 
 
