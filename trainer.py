d720ca1dd793fe5f03f583088f944612a911cf34
surface_pc <labpc2019@gmail.com>
2021-06-15 10:45:45 +0900

    add gan trainer

--- trainer.py
+++ trainer.py
@@ -111,8 +111,6 @@ class Trainer(object):
 
         train_output = np.array(train_output)
         val_output = np.array(val_output)
-        self._save_progress(train_output, mode='train')
-        self._save_progress(val_output, mode='val')
         return train_output, val_output
 
     def _trans_data(self, inputs, labels):
@@ -138,21 +136,9 @@ class Trainer(object):
         return self
 
     def _evaluate(self, output, label):
-        output = self._cut(output)
-        labels = self._cut(label)
+        output = torch.clamp(output,0., 1.)
+        labels = torch.clamp(label,0., 1.)
         return [self.psnr(labels, output).item(), self.ssim(labels, output).item(), self.sam(labels, output).item()]
-
-    def _cut(self, x):
-        x = torch.clamp(x, 0., 1.)
-        return x
-
-    def _save_progress(self, loss_progress: np.ndarray, *args,
-                       mode: str='train', **kwargs) -> None:
-
-        save_path = os.path.join(self.output_progress_path, f'{mode}.pkl')
-        with open(save_path, 'wb') as f:
-            pickle.dump(loss_progress, f)
-        return self
 
 
 class Deeper_Trainer(Trainer):
@@ -169,3 +155,112 @@ class Deeper_Trainer(Trainer):
             self.optimizer.step()
         show_loss = torch.nn.functional.mse_loss(output, labels)
         return show_loss
+
+
+class GANTrainer(Trainer):
+
+    def __init__(self, Gmodel, Dmodel, Gcriterion, Dcriterion,
+                 Goptim, Doptim, scheduler=None, callbacks=None, **kwargs):
+
+        self.Gmodel = Gmodel
+        self.Gcriterion = Gcriterion
+        self.Goptimizer = Goptimizer
+        self.Dmodel = Dmodel
+        self.Dcriterion = Dcriterion
+        self.Doptimizer = Doptimizer
+        self.scheduler = scheduler
+        self.callbacks = callbacks
+        self.psnr = PSNRMetrics().eval()
+        self.sam = SAMMetrics().eval()
+        self.ssim = SSIM().eval()
+        shape = kwargs.get('shape')
+        if shape is None:
+            shape = (64, 31, 48, 48)
+        self.zeros = torch.zeros(shape).to(device)
+        self.ones = torch.ones(shape).to(device)
+        self.colab_mode = kwargs.get('colab_mode', False)
+
+    def train(self, epochs, train_dataloader, val_dataloader, init_epoch=None):
+
+        if init_epoch is None:
+            init_epoch = 0
+        elif isinstance(init_epoch, int):
+            assert 'Please enter int to init_epochs'
+
+        if self.colab_mode is False:
+            _, columns = os.popen('stty size', 'r').read().split()
+            columns = int(columns)
+        else:
+            columns = 200
+        train_output = []
+        val_output = []
+        train_output_loss = []
+        val_output_loss = []
+
+        for epoch in range(init_epoch, epochs):
+            dt_now = datetime.now()
+            print(dt_now)
+            self.model.train()
+            mode = 'Train'
+            train_loss = []
+            val_loss = []
+            show_train_eval = []
+            show_val_eval = []
+            desc_str = f'{mode:>5} Epoch: {epoch + 1:05d} / {epochs:05d}'
+            with tqdm(train_dataloader, desc=desc_str, ncols=columns, unit='step', ascii=True) as pbar:
+                                                                                                              vblk'                for i, (inputs, labels) in enumerate(pbar):
+                    inputs, labels = self._trans_data(inputs, labels)
+                    loss, output = self._step(inputs, labels)
+                    train_loss.append(loss.item())
+                    show_loss = np.mean(train_loss)
+                    show_train_eval.append(self._evaluate(output, labels))
+                    show_mean = np.mean(show_train_eval, axis=0)
+                    evaluate = [f'{show_mean[0]:.7f}', f'{show_mean[1]:.7f}', f'{show_mean[2]:.7f}']
+                    self._step_show(pbar, Loss=f'{show_loss:.7f}', Evaluate=evaluate)
+                    torch.cuda.empty_cache()
+            show_mean = np.insert(show_mean, 0, show_loss)
+            train_output.append(show_mean)
+
+            mode = 'Val'
+            self.model.eval()
+            desc_str = f'{mode:>5} Epoch: {epoch + 1:05d} / {epochs:05d}'
+            with tqdm(val_dataloader, desc=desc_str, ncols=columns, unit='step', ascii=True) as pbar:
+                for i, (inputs, labels) in enumerate(pbar):
+                    inputs, labels = self._trans_data(inputs, labels)
+                    with torch.no_grad():
+                        loss, output = self._step(inputs, labels, train=False)
+                    val_loss.append(loss.item())
+                    show_loss = np.mean(val_loss)
+                    show_val_eval.append(self._evaluate(output, labels))
+                    show_mean = np.mean(show_val_eval, axis=0)
+                    evaluate = [f'{show_mean[0]:.7f}', f'{show_mean[1]:.7f}', f'{show_mean[2]:.7f}']
+                    self._step_show(pbar, Loss=f'{show_loss:.7f}', Evaluate=evaluate)
+                    torch.cuda.empty_cache()
+            show_mean = np.insert(show_mean, 0, show_loss)
+            val_output.append(show_mean)
+            if self.callbacks:
+                for callback in self.callbacks:
+                    callback.callback(self.model, epoch, loss=train_loss,
+                                      val_loss=val_loss, save=True, device=device, optim=self.optimizer)
+            if self.scheduler is not None:
+                self.scheduler.step()
+            print('-' * int(columns))
+
+        train_output = np.array(train_output)
+        val_output = np.array(val_output)
+        return train_output, val_output
+
+    def _trans_data(self, inputs, labels):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        return inputs, labels
+
+    def _step(self, inputs, labels, train=True):
+        if train is True:
+            self.optimizer.zero_grad()
+        output = self.model(inputs)
+        loss = self.criterion(output, labels)
+        if train is True:
+            loss.backward()
+            self.optimizer.step()
+        return loss, output
